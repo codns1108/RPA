e8e4377061d1745020d84b438dd7e7d6b358987c
codns1108 <codns1108@naver.com>
2024-09-12 12:31:32 +0900

    2주차실습

---
+++ 2week/ex00.py
@@ -0,0 +1,5 @@
+num1 = int(input("정수 1개 입력"))
+if(num1 % 2 == 0):
+    print("true")
+else:
+    print("false")
---
+++ 2week/ex4.py
@@ -0,0 +1,10 @@
+a =5
+if a==5:
+    print("right")
+    print('a is 5')
+if a==3:
+    print("right")
+    print('a is 3')
+if a != 3:
+    print('right')
+    print('a is not 3')
